33697f88ccbdfd59a5c8239e9172658a0c70c795
Dragan Vujanovic <sloncine@hotmail.com>
2020-01-30 23:17:17 +0100

    Add start shell script to start all servers simultaneously

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+SCRIPT=`realpath ${BASH_SOURCE[0]}`
+SCRIPTPATH=`dirname $SCRIPT`    
+
+listVar="Eureka Zuul Sellers Banka Acquirer BitCoin PayPal NC"
+for i in $listVar; do
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        terminal -e "cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run"
+    fi
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        osascript -e "tell application \"Terminal\" to do script \"cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run\""
+    fi
+    if [[ "$OSTYPE" == "windows" ]]; then
+        terminal -e "cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run"
+    fi
+done
