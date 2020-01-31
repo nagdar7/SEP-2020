b9790cc01102a66bacfd01597e4a1d88831524df
Dragan Vujanovic <sloncine@hotmail.com>
2020-01-31 11:43:46 +0100

    Fix start bash script for win

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ SCRIPTPATH=`dirname $SCRIPT`
 listVar="Eureka Zuul Sellers Banka Acquirer BitCoin PayPal NC"
 for i in $listVar; do
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        terminal -e "cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run"
+        terminal -e cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run
     fi
     if [[ "$OSTYPE" == "darwin"* ]]; then
         osascript -e "tell application \"Terminal\" to do script \"cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run\""
     fi
     if [[ "$OSTYPE" == "windows" ]]; then
-        terminal -e "cd $SCRIPTPATH\\$i && $SCRIPTPATH\\$i\\mvnw.cmd spring-boot:run"
+        terminal -e cd $SCRIPTPATH\\$i && $SCRIPTPATH\\$i\\mvnw.cmd spring-boot:run
     fi
 done
