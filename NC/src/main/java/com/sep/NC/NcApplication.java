75dc5104e7715f1df8daef31c8645b69fb4455a5
Tatjana995 <tanjazdravkovic1995@gmail.com>
2020-01-31 18:38:02 +0100

    conflict

--- NC/src/main/java/com/sep/NC/NcApplication.java
+++ NC/src/main/java/com/sep/NC/NcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NCApplication {
+public class NcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NCApplication.class, args);
+		SpringApplication.run(NcApplication.class, args);
 	}
 
 }
