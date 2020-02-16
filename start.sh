d01fdf2f6461bacbcfe795dd239dcc4a7ebddb6d
Dragan Vujanovic <sloncine@hotmail.com>
2020-02-16 21:18:19 +0100

    Use mkcert everywhere to generate valid cert

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ SCRIPTPATH=`dirname $SCRIPT`
 listVar="Banka Acquirer PCC Issuer NC Eureka Zuul Sellers PayPal BitCoin"
 for i in $listVar; do
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        terminal -e cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run
+        terminal -e cd $SCRIPTPATH/$i/src/main/resources/ && mkcert -pkcs12 localhost && cd ../../../ && ./mvnw spring-boot:run
     fi
     if [[ "$OSTYPE" == "darwin"* ]]; then
-        osascript -e "tell application \"Terminal\" to do script \"cd $SCRIPTPATH/$i && $SCRIPTPATH/$i/mvnw spring-boot:run\""
+        osascript -e "tell application \"Terminal\" to do script \"cd $SCRIPTPATH/$i/src/main/resources/ && mkcert -pkcs12 localhost && cd ../../../ && ./mvnw spring-boot:run\""
     fi
     if [[ "$OSTYPE" == "windows" ]]; then
-        terminal -e cd $SCRIPTPATH\\$i && $SCRIPTPATH\\$i\\mvnw.cmd spring-boot:run
+        terminal -e cd $SCRIPTPATH\\$i\\src\\main\\resources\\ && mkcert -pkcs12 localhost && cd ..\\..\\..\\ && .\\mvnw.cmd spring-boot:run
     fi
 done
 
